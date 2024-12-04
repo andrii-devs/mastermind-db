c00318c5c74ae18390deca334d75e62466df4261
Andrii Bohdan <andrewdevs@protonmail.com>
2024-12-04 22:20:01 +0100

    💾 chore(oops ): They should be ignored :|

--- build/auth/docker/init.sql
+++
@@ -1,6 +0,0 @@
-
-  CREATE DATABASE IF NOT EXISTS auth_db;
-  CREATE USER IF NOT EXISTS 'auth_user'@'%' IDENTIFIED BY 'auth_password';
-  GRANT ALL PRIVILEGES ON auth_db.* TO 'auth_user'@'%';
-  FLUSH PRIVILEGES;
-  
