e2c7c35a98ca53b76c6c8bf7151040a21fe7943e
Andrii Bohdan <andrewdevs@protonmail.com>
2024-12-08 22:47:11 +0100

    🐞 fix(templates): Resolved templates fixed path

--- src/helper/render-templates.helper.ts
+++ src/helper/render-templates.helper.ts
@@ -1,17 +1,22 @@
 import ejs from 'ejs';
 import fs from 'fs-extra';
 import path from 'path';
-import { getTemplatesDir } from './sequelize-blueprint-config.helper';
+import { logger } from '../utils/logger.utils';
 
 export async function renderTemplate(
   templatePath: string,
   outputPath: string,
   data: Record<string, any>,
 ) {
-  const templatesDir = getTemplatesDir();
-  const fullTemplatePath = path.join(templatesDir, templatePath);
-  const templateContent = await fs.readFile(fullTemplatePath, 'utf8');
+  const resolvedTemplatePath = path.resolve(
+    __dirname,
+    templatePath.startsWith('templates')
+      ? templatePath
+      : `../templates/${templatePath}`,
+  );
+
+  const templateContent = await fs.readFile(resolvedTemplatePath, 'utf8');
   const renderedContent = ejs.render(templateContent, data);
   await fs.outputFile(outputPath, renderedContent, 'utf8');
-  console.log(`Generated file: ${outputPath}`);
+  logger.success(`Generated file: ${outputPath}`);
 }
