da4a37d48905df264e8ce272caaca46e2d1e0644
Andrii Bohdan <andrewdevs@protonmail.com>
2024-12-12 02:56:18 +0100

    ✨ feat(helper): Implemented spinner and custom message logic

--- src/helper/run-sequelize-command.helper.ts
+++ src/helper/run-sequelize-command.helper.ts
@@ -1,18 +1,57 @@
 import { promisify } from 'util';
 import { exec } from 'child_process';
 import fs from 'fs-extra';
+import { handlerSequelizeMessage, logger } from '../utils/logger.utils';
+import path from 'path';
+import os from 'os';
+import { getRelativePath } from './sequelize-blueprint-config.helper';
+import { handlerError } from '../utils/error-handler.utils';
+import { createSpinner } from 'nanospinner';
 
 export const execAsync = promisify(exec);
 
-export async function runSequelizeCommand(command: string, path: string) {
+export async function runSequelizeCommand(
+  command: string,
+  servicePath: string,
+  environment: string,
+) {
+  const sequelizercPath = path.resolve(
+    servicePath,
+    'sequelize',
+    '.sequelizerc',
+  );
+
+  logger.info(
+    `Using .sequelizerc: ${getRelativePath(process.cwd(), sequelizercPath)}`,
+  );
+
+  const spinner = createSpinner(
+    `Running: npx sequelize-cli ${command} in ${getRelativePath(process.cwd(), servicePath)} with environment ${environment}`,
+  ).start();
+
   try {
-    console.log(`Running: npx sequelize-cli ${command} in ${path}`);
-    const { stdout } = await execAsync(`npx sequelize-cli ${command}`, {
-      cwd: path,
+    const { stdout } = await execAsync(
+      `export NODE_ENV=${environment} && npx sequelize-cli ${command} --options-path ${sequelizercPath}`,
+      {
+        cwd: servicePath,
+        env: {
+          ...process.env,
+          NODE_ENV: environment,
+        },
+        shell: os.platform() === 'win32' ? 'powershell.exe' : '/bin/bash',
+      },
+    );
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    stdout.split('\n').forEach((line) => {
+      handlerSequelizeMessage(line);
     });
-    console.log(stdout);
-  } catch (err) {
-    console.error(`Error running Sequelize command: ${err}`);
+
+    spinner.success('Done!');
+  } catch (err: any) {
+    spinner.stop();
+    handlerError(err, 'Database Migration');
   }
 }
 
