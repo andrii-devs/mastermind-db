ffe9a49ee12a869fcf429c72f384eeec760415bf
Andrii Bohdan <andrewdevs@protonmail.com>
2024-12-18 20:03:15 +0100

    🚄 perf(cli): Enhanced logic and split complexity between helper function

--- src/core/create-database.core.ts
+++ src/core/create-database.core.ts
@@ -3,17 +3,22 @@ import { DBType } from '../types';
 import { allocatePort } from '../helper/allocate-port.helper';
 import { scaffoldDatabase } from '../service/scaffold-database.service';
 import { scaffoldDockerCompose } from '../service/scaffold-docker-compose.service';
-import { scaffoldSequelizeFiles } from '../service/scaffold-sequelize-files.service';
+import { scaffoldSequelizeTemplate } from '../service/scaffold-sequelize-files.service';
 import { logger } from '../utils/logger.utils';
+import {
+  addOrUpdateProjectConfig,
+  loadProjectConfig,
+} from '../helper/mastermind-config.helper';
 
 export async function createDatabaseAction() {
+  const projectConfig = loadProjectConfig();
+  let { rootDir } = projectConfig;
+  if (!rootDir) {
+    logger.error(`No root directory specified. Please run init command first`);
+    process.exit(1);
+  }
+
   const { dbType, serviceName } = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'dbType',
-      message: 'Select the type of database you want to create:',
-      choices: Object.values(DBType),
-    },
     {
       type: 'input',
       name: 'serviceName',
@@ -21,22 +26,41 @@ export async function createDatabaseAction() {
       validate: (input) =>
         input.trim() !== '' ? true : 'Service name cannot be empty',
     },
+    {
+      type: 'list',
+      name: 'dbType',
+      message: 'Select the type of database you want to create:',
+      choices: Object.values(DBType),
+    },
   ]);
 
-  logger.info(
-    `Initializing a ${dbType} database for the "${serviceName}" service...`,
-  );
+  const { ormType } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'ormType',
+      message: `Select ORM for ${dbType}:`,
+      choices: ['Sequelize'],
+    },
+  ]);
 
   const dbName = `${serviceName}_db`;
   const port = await allocatePort(dbType);
   const databaseMap: Record<string, string> = {
     MySQL: 'mysql',
     PostgreSQL: 'postgres',
-    SQLite: 'sqlite',
   };
 
+  addOrUpdateProjectConfig(serviceName, ormType, rootDir);
+
   await scaffoldDatabase(serviceName, databaseMap[dbType], dbName, port);
-  await scaffoldSequelizeFiles(serviceName, []);
+
+  switch (ormType) {
+    case 'Sequelize':
+      await scaffoldSequelizeTemplate(serviceName);
+      break;
+    default:
+      throw new Error(`Unsupported ORM: ${ormType}`);
+  }
   await scaffoldDockerCompose(serviceName, databaseMap[dbType], port);
 
   logger.success(`Setup for "${serviceName}" complete!`);
