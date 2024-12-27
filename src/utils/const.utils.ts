731da093537c9b389dad3de227b93ddbbf7d85c4
Andrii Bohdan <andrewdevs@protonmail.com>
2024-12-27 15:54:05 +0100

    🎨 style(consts): Added label

--- src/utils/const.utils.ts
+++ src/utils/const.utils.ts
@@ -1,13 +1,18 @@
 import kleur from 'kleur';
 
+//First level menu items
 export const CREATE_SERVICE = 'Create a new service';
 export const MANAGE_EXISTING_SERVICE = 'Manage existing service';
 export const CONFIGURE_SETTINGS = 'Configuration';
+export const DELETE_SERVICE = 'Delete a service';
+
+// Second lvl menu items
 export const GENERATE_FILES =
   'Generate sequelize files (migrations/models/seeders):';
 export const MANAGE_MIGRATIONS = 'Manage migrations';
 export const MANAGE_SEEDERS = 'Manage seeders';
-export const EXIT_CLI = kleur.red('Exit');
 
+// Navigation menu items
+export const EXIT_CLI = kleur.red('Exit');
 export const GO_BACK_MAIN_MENU = 'Go back to the main menu';
 export const GO_BACK_SERVICE_MENU = 'Go back to the service menu';
