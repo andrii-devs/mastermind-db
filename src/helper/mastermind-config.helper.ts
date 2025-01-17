b9f384b7ab5b1c54dd779c89c36cc83c9f0867f2
Andrii Bohdan <andrewdevs@protonmail.com>
2025-01-17 19:02:49 +0100

    🔎 ref(config): Exported config type

--- src/helper/mastermind-config.helper.ts
+++ src/helper/mastermind-config.helper.ts
@@ -64,7 +64,7 @@ export function addOrUpdateProjectConfig(
   saveProjectConfig(projectConfig);
 }
 
-interface IConfigPath {
+export interface IConfigPath {
   rootDir: string;
   database: string;
   orm: string;
